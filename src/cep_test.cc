6a5c8ad7f464f482768a71175f37941fb93072c6
Anjiang-Wei <1020681930@qq.com>
2023-04-23 13:01:16 -0700

    add FSM implementation

--- src/cep_test.cc
+++ src/cep_test.cc
@@ -117,6 +117,90 @@ void simple(int k, const uint64_t * ts, const std::vector<std::vector<uint64_t>>
     }    
 }
 
+void print_vector(const std::vector<uint64_t>& vec) {
+    for (const auto& element : vec) {
+        std::cout << element << " ";
+    }
+    std::cout << std::endl;
+}
+
+void print_vec_vec(const std::vector<std::vector<uint64_t>>& vec) {
+    for (const auto& element : vec) {
+        print_vector(element);
+    }
+}
+
+std::vector<std::vector<uint64_t>> index_to_ts(std::vector<std::vector<uint64_t>> index, uint64_t* ts) {
+    std::vector<std::vector<uint64_t>> ret;
+    for (auto vec : index) {
+        std::vector<uint64_t> tmp;
+        for (auto i : vec) {
+            tmp.push_back(ts[i]);
+        }
+        ret.push_back(tmp);
+    }
+    return ret;
+}
+
+bool fsmaccept(std::vector<uint64_t> ts_vec) {
+    // given a sequence, use finite state machine to check if it is accepted
+    // the FSM only needs to maintain the previous timestamp element it has seen, and a counter (i)
+    for (int i = 0; i < ts_vec.size() - 1; i++) {
+        // time stamp should not decrease
+        if (ts_vec[i+1] < ts_vec[i]) {
+            return false;
+        }
+        // time stamp must be within the time window
+        if (ts_vec[i+1] > ts_vec[i]+DIFF) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void productImplement(std::vector<std::vector<uint64_t>> dimvalue,
+                    std::vector<std::vector<uint64_t>> &res,
+                    int layer,
+                    std::vector<uint64_t> tmp) {
+	if (layer < dimvalue.size() - 1){
+		for (int i = 0; i < dimvalue[layer].size(); i++){
+			std::vector<uint64_t> sb;
+			sb.clear();
+			for (int i = 0; i < tmp.size(); i++){
+				sb.push_back(tmp[i]);
+			}
+			sb.push_back(dimvalue[layer][i]);
+			productImplement(dimvalue, res, layer+1,sb);
+		}
+	}
+	else if (layer == dimvalue.size() - 1){
+		for (int j = 0; j < dimvalue[layer].size();j++){
+            tmp.push_back(dimvalue[layer][j]);
+			res.push_back(tmp);
+			tmp.pop_back();
+		}
+	}
+}
+
+// this is FSM equivalent of the simple function
+void fsm(int k, uint64_t* ts, std::vector<std::vector<uint64_t>> conditions,
+         std::vector<std::vector<uint64_t>>& ret) {
+   // compute catesian product for condition arrays as index arrays' catesian product
+   std::vector<std::vector<uint64_t>> catesian_product;
+   std::vector<uint64_t> tmp;
+   productImplement(conditions, catesian_product, 0, tmp);
+   // print_vec_vec(catesian_product);
+   // printf("xxxxxxxx\n");
+   // use the index product to generate ts product, used for FSM input to be filtered
+   std::vector<std::vector<uint64_t>> ts_product = index_to_ts(catesian_product, ts);
+   for (int i = 0; i < ts_product.size(); i++) {
+       if (fsmaccept(ts_product[i])) {
+           ret.push_back(catesian_product[i]);
+       }
+   }
+}
+
+
 int main()
 {
     // generate a test case for the function simple
@@ -139,4 +223,8 @@ int main()
         }
         std::cout << std::endl;
     }
+    std::cout << "---------" << std::endl;
+    std::vector<std::vector<uint64_t>> ret2;
+    fsm(3, ts, conditions, ret2);
+    print_vec_vec(ret2);
 }
